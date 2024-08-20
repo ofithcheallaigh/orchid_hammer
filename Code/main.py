869354c6f7cd6115f3585c94b9034721568c0437
ofithcheallaigh <sofithcheallaigh@gmail.com>
2024-08-20 22:00:53 +0100

    Updated code for text gen, also, converted to functions

--- Code/main.py
+++ Code/main.py
@@ -1,7 +1,20 @@
 from transformers import pipeline
 
-classifier = pipeline("zero-shot-classification")
-result = classifier("Keir Starmer is the PM in the UK",
-                candidate_labels=["education", "politics", "business"])
+def main():
+    classifier = pipeline("zero-shot-classification")
+    result = classifier("Keir Starmer is the PM in the UK",
+                    candidate_labels=["education", "politics", "business"])
 
-print(result)
+    print(result)
+
+
+def generator():
+    generator = pipeline("text-generation", model="gpt2")
+    result = generator("In this course, we will teach you how to")
+
+    print(result)
+
+
+if __name__ == "__main__":
+    # main()
+    generator()
