bc230657ae1ddf0e2c734c35496acf09b81ee5ff
ofithcheallaigh <sofithcheallaigh@gmail.com>
2024-08-21 22:21:42 +0100

    Adding updated code to use new model in text generation

--- Code/main.py
+++ Code/main.py
@@ -14,7 +14,17 @@ def generator():
 
     print(result)
 
+def generator_distilpgt2():
+    generator = pipeline("text-generation", model="distilgpt2")
+    result = generator("In this course, we will teach you how to", 
+              max_length=30,
+              num_return_sequences=2
+              )
+    
+    print(result)
+
 
 if __name__ == "__main__":
     # main()
-    generator()
+    # generator()
+    generator_distilpgt2()
