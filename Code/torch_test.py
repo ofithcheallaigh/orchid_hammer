6ffa27e35539f528215b36e86f077772f8536961
sofithcheallaigh <sofithcheallaigh@sensata.com>
2024-08-28 09:02:40 +0100

    Updated scratch pad

--- Code/torch_test.py
+++ Code/torch_test.py
@@ -1,3 +1,10 @@
 import torch
-print(torch.__version__)
+import torchvision
+from transformers import pipeline
+
+print(f"torch version: {torch.__version__}")
+print(f"torchvision version: {torchvision.__version__}")
+
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+print(summarizer("This is a test summary."))
 
